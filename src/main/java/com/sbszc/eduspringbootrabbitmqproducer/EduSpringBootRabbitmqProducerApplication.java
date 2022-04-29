3ec6ef700b4e4d14c46446cd23dfeb46a1194a9b
sbszc <sebastian.zcc@gmail.com>
2022-04-28 19:18:31 -0500

    initial commit

---
+++ src/main/java/com/sbszc/eduspringbootrabbitmqproducer/EduSpringBootRabbitmqProducerApplication.java
@@ -0,0 +1,13 @@
+package com.sbszc.eduspringbootrabbitmqproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduSpringBootRabbitmqProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduSpringBootRabbitmqProducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sbszc/eduspringbootrabbitmqproducer/EduSpringBootRabbitmqProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbszc.eduspringbootrabbitmqproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduSpringBootRabbitmqProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
